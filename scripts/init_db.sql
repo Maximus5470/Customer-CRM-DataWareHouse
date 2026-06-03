9d86e6728581249cf887c7ca17e8323af9cf6b93
Maximus5470 <gauthamsharma2005@gmail.com>
2026-06-03 19:50:53 +0530

    Add initial dataset and database initialization script
    - Created a new CSV file `PX_CAT_G1V2.csv` containing product categories, subcategories, and maintenance information for accessories, bikes, clothing, and components.
    - Added a new SQL script `init_db.sql` to initialize the Customer CRM Data Warehouse, including dropping the existing database if it exists and creating new schemas for bronze, silver, and gold layers.

---
+++ scripts/init_db.sql
@@ -0,0 +1,25 @@
+use master;
+go
+
+-- creating a fresh database for the Customer CRM Data Warehouse. If it already exists, we will drop it and create a new one.
+if exists(select * from sys.databases where name = 'CustomerCRM_DWH')
+begin
+	alter database CustomerCRM_DWH set single_user with rollback immediate;
+	drop database CustomerCRM_DWH;
+end;
+go
+
+create database CustomerCRM_DWH;
+go
+
+use CustomerCRM_DWH;
+go
+
+
+-- creating schemas for the data warehouse layers
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
